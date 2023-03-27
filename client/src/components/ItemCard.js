103a75b6bc95b2416105d4635cbeaea095a618f5
Justin Tan <jtjtjt703@gmail.com>
2023-03-27 15:09:29 +0800

    added placeholder ui for adding, deleting, editing items

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -1,20 +1,38 @@
 import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
 
 function ItemCard({ item, altText }) {
+  const [showButtons, setShowButtons] = useState(false);
+
+  function handleMouseEnter() {
+    setShowButtons(true);
+  }
+
+  function handleMouseLeave() {
+    setShowButtons(false);
+  }
+
   return (
     <Card className="shadow border h-100 m-1">
       {item === undefined ? (
-        <div className="h-100 container d-flex justify-content-center align-items-center">
-          <div className="align-items-center">
-            <Button variant="primary">{altText}</Button>
+        <a href="#" className="h-100">
+          <div className="h-100 container d-flex rounded justify-content-center align-items-center border border-primary">
+            <div className="align-items-center text-center ">
+              {altText}
+              <h2 class="bi bi-plus-lg"></h2>
+            </div>
           </div>
-        </div>
+        </a>
       ) : (
-        <>
+        <div
+          className="w-100 h-100"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           <Card.Img
             src={item?.image}
             alt="Card image"
-            className="w-100 h-100 rounded"
+            className="w-100 h-100"
             style={{ objectFit: "cover" }}
           />
           <Card.ImgOverlay className="h-25 bg-light opacity-75 m-2">
@@ -26,7 +44,19 @@ function ItemCard({ item, altText }) {
               <small>{item?.description}</small>
             </p>
           </Card.ImgOverlay>
-        </>
+          {showButtons && (
+            <div className="position-absolute bottom-0 end-0 m-2">
+              <button
+                className="btn  btn-primary me-2 rounded-circle bi bi-pencil-fill"
+                style={{ height: "3rem", width: "3rem" }}
+              />
+              <button
+                className="btn btn-danger rounded-circle bi bi-trash3-fill"
+                style={{ height: "3rem", width: "3rem" }}
+              />
+            </div>
+          )}
+        </div>
       )}
     </Card>
   );
