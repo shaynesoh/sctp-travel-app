36645600de41a9a3e0cb8bd3e0b8f952237176d6
Justin Tan <jtjtjt703@gmail.com>
2023-03-27 20:48:25 +0800

    added Delete Trip confirmation dialog

---
+++ client/src/components/DeleteTripModal.js
@@ -0,0 +1,23 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/esm/Button";
+
+function DeleteTripModal({ show, handleClose, handleDelete }) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm Deletion</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to delete this trip?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={handleDelete}>
+          Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default DeleteTripModal;
--- client/src/components/TripList.js
+++ client/src/components/TripList.js
@@ -1,40 +1,70 @@
-import React from 'react';
-import Button from 'react-bootstrap/esm/Button';
-import { Link } from 'react-router-dom';
+import { React, useState } from "react";
+import Button from "react-bootstrap/esm/Button";
+import { Link } from "react-router-dom";
+import DeleteTripModal from "./DeleteTripModal";
 
-function TripList({ name, country, budget, startDate, endDate, index, setTripList, deleteTrip }) {
+function TripList({
+  name,
+  country,
+  budget,
+  startDate,
+  endDate,
+  index,
+  setTripList,
+  deleteTrip,
+}) {
+  const [confirmationVisible, setConfirmationVisible] = useState(false);
 
   const handleDelete = () => {
     deleteTrip(index);
-  }
-  
+    setConfirmationVisible(false);
+  };
+
+  const showConfirmation = () => setConfirmationVisible(true);
+
+  const hideConfirmation = () => setConfirmationVisible(false);
+
   return (
     <>
-        <div className="col-xl-4 col-sm-6 mb-3">
-          <div className="card h-100 shadow-sm bg-white rounded">
-            <div className="card-body">
-              <h2 className="mb-3">{name}</h2>
-              <div className="d-flex">
-                <div className="col-6">
-                  <div className="mr-5">{country}</div>
-                  <div className="mr-5">{startDate} to {endDate}</div>
-                </div>
-                <div className="col-6">
-                  <div className="mr-5">$ {budget}</div>
+      <div className="col-xl-4 col-sm-6 mb-3">
+        <div className="card h-100 shadow-sm bg-white rounded">
+          <div className="card-body">
+            <h2 className="mb-3">{name}</h2>
+            <div className="d-flex">
+              <div className="col-6">
+                <div className="mr-5">{country}</div>
+                <div className="mr-5">
+                  {startDate} to {endDate}
                 </div>
               </div>
+              <div className="col-6">
+                <div className="mr-5">$ {budget}</div>
+              </div>
             </div>
-            <div className="card-footer clearfix small z-1 d-flex align-items-center justify-content-between" href="#">
+          </div>
+          <div
+            className="card-footer clearfix small z-1 d-flex align-items-center justify-content-between"
+            href="#"
+          >
             <div>
-              <span className="float-left"><Link to={`/trips/${index}`}>View Details</Link> </span>
+              <span className="float-left">
+                <Link to={`/trips/${index}`}>View Details</Link>{" "}
+              </span>
               <span className="float-right">
                 <i className="fa fa-angle-right"></i>
               </span>
             </div>
-            <Button variant="dark" onClick={handleDelete}>Delete</Button>
-            </div>
+            <Button variant="dark" onClick={showConfirmation}>
+              Delete
+            </Button>
           </div>
         </div>
+      </div>
+      <DeleteTripModal
+        show={confirmationVisible}
+        handleClose={hideConfirmation}
+        handleDelete={handleDelete}
+      />
     </>
   );
 }
