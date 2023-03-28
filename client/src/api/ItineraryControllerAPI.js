cc625be96e5b1aabdf64651b2bcb268adf066e8d
Justin Tan <jtjtjt703@gmail.com>
2023-03-28 16:05:21 +0800

    added updateItinerary endpoint

--- client/src/api/ItineraryControllerAPI.js
+++ client/src/api/ItineraryControllerAPI.js
@@ -1,12 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 const itineraryApi = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: "http://localhost:8080",
 });
 
 const itineraryService = {
   async getAllItineraries() {
-    const response = await itineraryApi.get('/itineraries');
+    const response = await itineraryApi.get("/itineraries");
+    return response.data;
+  },
+  async addItinerary(itinerary) {
+    const response = await itineraryApi.post("/itineraries", itinerary);
     return response.data;
   },
   async getUserItinerary(userId) {
@@ -17,28 +21,43 @@ const itineraryService = {
     const response = await itineraryApi.get(`/itineraries/${itineraryId}`);
     return response.data;
   },
-  async getItineraryItems(itineraryId) {
-    const response = await itineraryApi.get(`/itineraries/${itineraryId}/items`);
-    return response.data;
-  },
-  async addItinerary(itinerary) {
-    const response = await itineraryApi.post('/itineraries', itinerary);
-    return response.data;
-  },
-  async addItineraryItem(itineraryId, itineraryItem) {
-    const response = await itineraryApi.post(`/itineraries/${itineraryId}`, itineraryItem);
-    return response.data;
-  },
-  async deleteItineraryItem(itineraryItemId) {
-    const response = await itineraryApi.delete(`/itineraries/items/${itineraryItemId}`);
+  async updateItinerary(itineraryId, itinerary) {
+    const response = await itineraryApi.put(
+      `/itineraries/${itineraryId}`,
+      itinerary
+    );
     return response.data;
   },
   async deleteItinerary(itineraryId) {
     const response = await itineraryApi.delete(`/itineraries/${itineraryId}`);
     return response.data;
-  }
-
-
+  },
+  async getItineraryItems(itineraryId) {
+    const response = await itineraryApi.get(
+      `/itineraries/${itineraryId}/items`
+    );
+    return response.data;
+  },
+  async addItineraryItem(itineraryId, itineraryItem) {
+    const response = await itineraryApi.post(
+      `/itineraries/${itineraryId}`,
+      itineraryItem
+    );
+    return response.data;
+  },
+  async updateItineraryItem(itineraryItemId, itineraryItem) {
+    const response = await itineraryApi.put(
+      `/itineraries/items/${itineraryItemId}`,
+      itineraryItem
+    );
+    return response.data;
+  },
+  async deleteItineraryItem(itineraryItemId) {
+    const response = await itineraryApi.delete(
+      `/itineraries/items/${itineraryItemId}`
+    );
+    return response.data;
+  },
 };
 
 export default itineraryService;
