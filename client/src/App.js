008b4dae1456eb1f8e6c6d732e897264c4660eab
Justin Tan <jtjtjt703@gmail.com>
2023-03-30 01:23:04 +0800

    added user login authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,111 @@
-import 'font-awesome/css/font-awesome.min.css';
-import './assets/css/app.css';
-import TripsOverview from './pages/TripsOverview';
-import LoginPage from './pages/auth/LoginPage'
-import ResetPassword from './pages/auth/ResetPassword';
-import ProfilePage from './pages/profile/ProfilePage';
-import ChangePasswordPage from './pages/profile/ChangePasswordPage';
-import TripDetails from './pages/TripDetails';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import "font-awesome/css/font-awesome.min.css";
+import "./assets/css/app.css";
+import TripsOverview from "./pages/TripsOverview";
+import LoginPage from "./pages/auth/LoginPage";
+import ResetPassword from "./pages/auth/ResetPassword";
+import ProfilePage from "./pages/profile/ProfilePage";
+import ChangePasswordPage from "./pages/profile/ChangePasswordPage";
+import TripDetails from "./pages/TripDetails";
+
+import { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : null
+    );
+
+
+
+  const saveToken = (token) => {
+    localStorage.setItem("token", token);
+    setToken(token);
+  };
+
+
+
+  function RequireAuth({ children }) {
+
+    console.log(token);
+    if (!token) {
+      console.log("huh");
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  }
+
   return (
-        <Router>
-            <Routes>
-                <Route exact path='/' element={<LoginPage/>} />
-                <Route exact path='/trips' element={<TripsOverview/>} />
-                <Route exact path='/login' element={<LoginPage/>} />
-                <Route exact path='/reset-password' element={<ResetPassword/>} />
-                <Route exact path='/profile' element={<ProfilePage/>} />
-                <Route exact path='/change-password' element={<ChangePasswordPage/>} />
-                <Route exact path="/trips/:index" element={<TripDetails />} />
-            </Routes>  
-        </Router>
-    )
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/login"
+          element={<LoginPage setToken={saveToken} />}
+        />
+        <Route
+          exact
+          path="/"
+          element={
+            <RequireAuth>
+              <TripsOverview />
+            </RequireAuth>
+          }
+        />
+        <Route
+          exact
+          path="/trips"
+          element={
+            <RequireAuth>
+              <TripsOverview />
+            </RequireAuth>
+          }
+        />
+        <Route
+          exact
+          path="/reset-password"
+          element={
+            <RequireAuth>
+              <ResetPassword />
+            </RequireAuth>
+          }
+        />
+        <Route
+          exact
+          path="/profile"
+          element={
+            <RequireAuth>
+              <ProfilePage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          exact
+          path="/change-password"
+          element={
+            <RequireAuth>
+              <ChangePasswordPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          exact
+          path="/trips/:index"
+          element={
+            <RequireAuth>
+              <TripDetails />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/pages/auth/LoginPage.js
+++ client/src/pages/auth/LoginPage.js
@@ -1,54 +1,109 @@
-import React from "react";
-import "../../assets/css/login.css"
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import authService from "../../api/AuthAPI";
 import authLayout from "../../hoc/authLayout";
 
-class LoginPage extends React.Component {
-    constructor(props){
-        super(props);
+function LoginPage({ setToken }) {
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
 
-        this.state = {};
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setCredentials((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { username, password } = credentials;
+      // validate username and password here
+      const token = await authService.authenticateUser(username, password);
+      console.log(token);
+      if (token) {
+        setToken(token);
+        navigate("/");
+      } else {
+        console.error("Authentication failed");
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    render(){
-        return <>
-            <form className="login-form">
-                <div className="d-flex align-items-center my-4">
-                    <h1 className="text-center fw-normal mb-0 me-3">Sign In</h1>
-                </div>
-                {/* <!-- Email input --> */}
-                <div className="form-outline mb-4">
-                    <label className="form-label" htmlFor="form3Example3">Email address</label>
-                    <input type="email" id="form3Example3" className="form-control form-control-lg"
-                    placeholder="Enter a valid email address" />
-                </div>
+  return (
+    <>
+      <form className="login-form">
+        <div className="d-flex align-items-center my-4">
+          <h1 className="text-center fw-normal mb-0 me-3">Sign In</h1>
+        </div>
+        {/* <!-- Email input --> */}
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="form3Example3">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="form3Example3"
+            className="form-control form-control-lg"
+            placeholder="Enter a valid email address"
+            name="username"
+            value={credentials.username}
+            onChange={handleInputChange}
+          />
+        </div>
 
-                {/* <!-- Password input --> */}
-                <div className="form-outline mb-3">
-                    <label className="form-label" htmlFor="form3Example4">Password</label>
-                    <input type="password" id="form3Example4" className="form-control form-control-lg"
-                    placeholder="Enter password" />
-                </div>
+        {/* <!-- Password input --> */}
+        <div className="form-outline mb-3">
+          <label className="form-label" htmlFor="form3Example4">
+            Password
+          </label>
+          <input
+            type="password"
+            id="form3Example4"
+            className="form-control form-control-lg"
+            placeholder="Enter password"
+            name="password"
+            value={credentials.password}
+            onChange={handleInputChange}
+          />
+        </div>
 
-                <div className="d-flex justify-content-between align-items-center">
-                    {/* <!-- Checkbox --> */}
-                    <div className="form-check mb-0">
-                    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                    <label className="form-check-label" htmlFor="form2Example3">
-                        Remember me
-                    </label>
-                    </div>
-                    <Link to="/reset-password" className="text-body">Forgot password?</Link>
-                </div>
+        <div className="d-flex justify-content-between align-items-center">
+          {/* <!-- Checkbox --> */}
+          <div className="form-check mb-0">
+            <input
+              className="form-check-input me-2"
+              type="checkbox"
+              value=""
+              id="form2Example3"
+            />
+            <label className="form-check-label" htmlFor="form2Example3">
+              Remember me
+            </label>
+          </div>
+          <Link to="/reset-password" className="text-body">
+            Forgot password?
+          </Link>
+        </div>
 
-                <div className="text-center text-lg-start mt-4 pt-2">
-                    <Link to="/trips" type="button" className="btn btn-primary btn-lg">Login</Link>
-                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                        className="link-danger">Register</a></p>
-                </div>
-            </form>
-        </>
-    }
+        <div className="text-center text-lg-start mt-4 pt-2">
+          <Button className="btn btn-primary btn-lg" onClick={handleSubmit}>
+            Login
+          </Button>
+          <p className="small fw-bold mt-2 pt-1 mb-0">
+            Don't have an account?{" "}
+            <a href="#!" className="link-danger">
+              Register
+            </a>
+          </p>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default authLayout(LoginPage);
